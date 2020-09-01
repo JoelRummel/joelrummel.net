6234bd7453f264e95bb89c7418dea2b380e48ac5
JoelRummel <joelrummel@yahoo.com>
2020-08-31 21:33:57 -0400

    Split out header/footer

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php require_once("init.php"); ?>
+
+<div id="LandingHeader">
+	<h1>Hi, my name's Joel Rummel.</h1>
+	<p>I'm a Junior studying Computer Science at the University of Michigan. From developing web apps, to video games,
+		to autonomous robots, I've accumulated a wide range of programming experience. Take a look at some of my personal
+		and professional projects.</p>
+</div>
+<div id="FourSquares">
+	<div id="FullStackAppsSquare" class="Square">
+		<h2 class="SquareHeader">Full Stack Applications</h2>
+		<p class="SquareBody">This last summer, I interned at Spencer Manufacturing Inc. as a full-stack software
+			developer. I created a cross-platform app that integrated project management with timekeeping, instant
+			messaging, issue tracking, and more. The app was custom-built for private use throughout the company.</p>
+	</div>
+	<div id="RoboticsSquare" class="Square">
+		<h2 class="SquareHeader">Robotics</h2>
+		<p class="SquareBody">Making a robot move on its own is easy enough, but making it move in a perfectly straight
+			line certainly isn't. In high school, I served as the lead programmer of our FRC robotics team, programming
+			sensor-based autonomous routines for robots built on omnidirectional drive bases.</p>
+	</div>
+	<div id="DataAnalysisSquare" class="Square">
+		<h2 class="SquareHeader">Data Analysis Tools</h2>
+		<p class="SquareBody">While I was a member of my high school's robotics team, I wrote software that interacted
+			with an API to "scout" our opponents and analyze their strengths and weaknesses. This program landed me my
+			first internship at Whirlpool, where I helped develop several data analysis tools for quality analysts and
+			engineers.
+		</p>
+	</div>
+	<div id="VideoGamesSquare" class="Square">
+		<h2 class="SquareHeader">Video Games</h2>
+		<p class="SquareBody">In collaboration with my brother <a href="https://alexrummel.com">Alex</a>, I've
+			participated in a number of competitive "game jams", where participants must develop a video game in a very
+			short time span. Our latest two games, "Liquid Bacon" and "The Empty Cone", earned especially high marks - the
+			former game earning 1st place out of over 25 entries!</p>
+	</div>
+</div>
+<div id="BlurbsSection">
+	<div class="BlurbBox">
+		<h2 class="BlurbHeader">No language or framework too big.</h2>
+		<p class="BlurbBody">I strongly believe that it is important for a programmer to be flexible, in order to be able
+			to use the best tools for the job. In each year of my internships, I have been able to quickly pick up a
+			new framework or technology in order to most efficiently tackle the issue at hand.</p>
+	</div>
+	<div class="BlurbBox">
+		<h2 class="BlurbHeader">Driven by challenge.</h2>
+		<p class="BlurbBody">The bigger the project, the better. My passion has always lied in creation, no matter the
+			form.</p>
+	</div>
+	<div class="BlurbBox">
+		<h2 class="BlurbHeader">Communication? Not a problem.</h2>
+		<p class="BlurbBody">I have developed strong communication and collaboration skills through my years of teamwork
+			and professional experience in both high school and college. Presentation skills are also a strong point thanks
+			to presenting to multiple Whirlpool vice presidents. I have even been entrusted by each company to continue my
+			work for them remotely during the school year.</p>
+	</div>
+</div>
+
+<?php require_once("footer.php"); ?>
---
+++ init.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@200&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
