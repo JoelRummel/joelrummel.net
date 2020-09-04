fde2d640f68df0c737e5d1fde47111da1fab968f
JoelRummel <joelrummel@yahoo.com>
2020-09-04 11:25:13 -0400

    added tabs

--- init.php
+++ init.php
@@ -1,3 +1,5 @@
+<?php $page = basename($_SERVER["SCRIPT_FILENAME"], '.php'); ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -13,5 +15,16 @@
 		<a href="/" style="text-decoration: none">
 			<h2>Joel Rummel</h2>
 		</a>
+		<div id="NavBar">
+			<a class="<?php echo ($page == "/" || $page == "index") ? "NavTabCurrent" : "NavTab" ?>" href="/">
+				<h3>About</h3>
+			</a>
+			<a class="<?php echo ($page != "/" && $page != "index") ? "NavTabCurrent" : "NavTab" ?>" href="#">
+				<h3>My Stuff</h3>
+			</a>
+			<a class="<?php echo ($page == "resume") ? "NavTabCurrent" : "NavTab" ?>" href="#">
+				<h3>Resume</h3>
+			</a>
+		</div>
 	</div>
 	<div id="Content">
