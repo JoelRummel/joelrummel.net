3c89308067bc7e7d98f74bf391a19cc1c6974916
JoelRummel <joelrummel@yahoo.com>
2020-09-11 13:30:01 -0400

    Added robotics images and changed wording

--- robotics.php
+++ robotics.php
@@ -4,7 +4,8 @@
 <div class="IntroImageWrapper">
 	<img src="images/RobotFront.png" class="IntroImage" />
 </div>
-<p>I was a member of my high school FRC robotics team (4855 Ramageddon) from 8th grade through senior year. In my final
+<p>I was a member of my high school FRC robotics team (4855 Ramageddon) from 8th grade through senior year. My job was
+	to write programs that operated competitive robots, both autonomously and via human input. In my final
 	three years, I served as the team's lead programmer, switching our language from LabVIEW to Java and mentoring
 	underclassmen.
 </p>
@@ -13,13 +14,17 @@
 	as a "swerve drive". This type of drive base has four wheels - one on each corner - that can each independently pivot
 	to any direction. That means eight motors, four encoders (minimum), and a whole ton of programming challenges.
 	So, over the summer, I loaded a swerve drive prototype into the trunk of my car, and took it home to experiment with.
-	After three days of non-stop research and programming, I had written a fully-functioning program to control the swerve
-	drive with two joysticks.
+	After three days of non-stop researching and programming, I had written a fully-functioning program to control the swerve
+	drive with two joysticks on an XBox controller.
 </p>
 <p>We went on to use a swerve base on our 2018 robot, and the competitive edge propelled us to both the state and world
 	championship events for the first time in our team's history. At the world championship event, I was honored to serve
-	as the alliance captain (leader and representative) of four different teams through the playoffs, where we lost
-	to the eventual world champions.
+	as the alliance captain (leader and representative) of four different teams through the playoffs, where we were eliminated
+	by the eventual world champions.
 </p>
+<div class="Gallery">
+	<img src="images/RobotImage.jpg" />
+	<img src="images/TeamImage.jpg" />
+</div>
 
 <?php require_once("footer.php"); ?>
