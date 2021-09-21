696f8a3eb42c0010bc2ca0c39a770ff26ecccc34
joelrummel <joelrummel@yahoo.com>
2021-09-21 16:58:07 -0400

    Add long descriptions for the video games

--- src/data/projectExperience.js
+++ src/data/projectExperience.js
@@ -5,7 +5,14 @@ const PROJECT_EXPERIENCE = [
         startDate: "March 2020",
         image: require('../resources/experiences/emptycone.png'),
         shortDescription: "I programmed a video game in 72 hours for the worldwide Ludum Dare game jam, placing in the top 8% of over 3,500 competing entries",
-        longDescription: `Long description here`,
+        longDescription: `In March 2020, my brother and I participated in the 46th Ludum Dare game jam, creating "The Empty Cone".\
+            My brother took charge of the graphics, interfaces, and audio, while I wrote most of the code. The theme was "Keep It\
+            Alive", so in "The Empty Cone" you must keep your ice cream business alive. That is, both finanically alive from bankruptcy,
+            and literally alive from random monster attacks! The game placed in the top 8% out of about 3,500 entries overall.
+            
+            The Empty Cone was made with Godot and written in Godot's scripting language GDScript. It was built for Windows and
+	        for the web, which means you can play it in your browser <a href="https://acr515.itch.io/ld46">here</a>.
+            `,
         skills: [
             {
                 label: "Tools/skills",
@@ -45,7 +52,15 @@ const PROJECT_EXPERIENCE = [
         startDate: "Nov 2019",
         image: require('../resources/experiences/liquidbacon.png'),
         shortDescription: "I programmed a video game in one week for a game jam, placing 1st out of 26 competing entries.",
-        longDescription: "Long description here.",
+        longDescription: `In November 2018, my brother and I created "Liquid Bacon" in one week for the "GameMaker Community\
+        Jam". Built to satisfy the theme "Chemical Reactions", this game follows a stingy protagonist who would rather try and\
+        find the recipe for a mass-produced product than simply buy it. The player does so by talking to strange characters,\
+        wandering remote areas, pillaging trash cans, fighting off bears, and exploring sketchy warehouses. The game was voted\
+        as the winner out of all 26 entries!
+    
+        Liquid Bacon was made in GameMaker: Studio and written in GameMaker's proprietary scripting language GML.\
+        It was built for Windows. (Link to game coming soon)
+        `,
         skills: [
             {
                 label: "Tools/skills",
