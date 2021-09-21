567df4e0826fe467451e34735872fb6b2742a0d1
joelrummel <joelrummel@yahoo.com>
2021-09-21 14:25:43 -0400

    Wrote long descriptions for whirlpool and spencer

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -158,8 +158,48 @@ const WORK_EXPERIENCE = [
         startDate: "June 2020",
         endDate: "August 2020",
         image: require('../resources/experiences/spencerapp.png'),
-        shortDescription: "I developed an internal app for web and mobile to serve as the sljf sdl fksjf lskfjs fewfiajs ds xmvn x,mnsw oefjsdfj slfjslefjsdkfj slk.",
-        longDescription: "Long description here.",
+        shortDescription: "I developed an internal app for web and mobile to replace the company's old timekeeping and project management solutions.",
+        longDescription: `Over the summer of 2020, I worked at Spencer Manufacturing Inc, a custom firetruck manufacturer. As you can\
+            probably imagine, a company in such a niche industry had a lot of niche needs, especially in software - a great opportunity\
+            for me to use my skillset and do some problem-solving. They challenged me to create a piece of software that would:
+
+              -Replace their existing timekeeping solution, which was expensive and didn't provide detailed insight
+              -Replace their existing group chat solution, which was poorly suited for per-project communication
+              -Create a coherent and integrated system for issue tracking on each project
+              -Provide custom analyses to answer questions such as "which parts of what projects take us longer than expected?"
+
+            The result of this challenge was an app called "Truck Manager" that tackled all of this and more. Built with a\
+            React Native front-end and a PHP/MySQL back-end, the app was deployed to iOS, Android, and web browsers.
+
+            In Truck Manager, each project (or "truck") is listed on the home page. And each truck has its own task list, chat room,\
+            and issue tracker. Each task in the task list can be clocked into, or have an issue reported on it. Supervisors can also\
+            mark tasks as completed, which removes them from the list.
+
+            Every element of a truck is integrated with one another, which means that reporting an issue on a task:
+
+              -adds that task to the issue tracker, and
+	          -sends an alert in the chat room, which
+	          -sends a push notification to anyone subscribed to that truck.
+
+            A more complete set of features includes:
+
+	          -User login via email and password, or via four-digit PIN if the device is a registered company tablet
+	          -Accounts that can be one of three different roles (Employee, Supervisor, and Administrator), the latter roles\
+            providing more permissions
+	          -Image upload functionality for chat rooms and for issue tracking
+	          -Tracking of unread chat messages for each individual truck
+	          -"Add Truck" menu that allows a truck to be created simply by uploading a .csv file containing truck details
+	          -Restricted "People" menu that allows administrators to add/remove employees, view and edit employee timesheets\
+            by week, and export employee timesheets that can be imported by QuickBooks
+	          -"Announcement" system that notifies all employees, and forces them to acknowledge the announcement before using the app
+	          -Shared Calendar system that allows tracking of events such as project deadlines and truck delivery dates
+	          -Restricted "analysis" window that describes which tasks took longer or shorter times than expected to complete,\
+            and breaks down who worked for how long on them
+
+            In terms of complexity, this project remains one of my biggest undertakings to date. From authentication and\
+            timekeeping on the back-end to calendar and chat interfaces on the front-end, this internship gave me a ton of\
+            experience in full stack, big-picture software development that I have already been able to make great use of.
+            `,
         skills: [
             {
                 label: "Tech",
@@ -195,21 +235,32 @@ const WORK_EXPERIENCE = [
         endDate: "August 2019",
         image: require('../resources/experiences/whirlpoollogo.jpg'),
         shortDescription: "I worked with a subteam in the Quality department to build data analysis apps and tools for internal use by engineers and data analysts.",
-        longDescription: "Long description here.",
+        longDescription: `I worked for the Whirlpool Quality department for two straight summers from 2018 - 2019. While I was\
+        there, I worked on a number of data analysis tools for internal use by company quality analysts and engineers. These tools\
+        largely consisted of web apps with a backend written in Google Apps Script, and a frontend written in either vanilla\
+        JavaScript or React.js.
+        
+        In my first year, I independently proposed, and subsequently developed, a web app called the "Claims Coding Tool",\
+	    which was intended to help improve the efficiency of data entry by Whirlpool's claim coding team based in India.\
+	    I worked directly with this team to develop the app, which I later presented to multiple company vice presidents.
+
+        My second year consisted of development on existing data analysis tools, as well as the creation of a new tool\
+	    called "Go-And-See". This app allows employees to subscribe to various quality data streams, and to apply custom\
+	    filters on them. They would then receive emails containing the most recent dataset on a user-defined interval.
+
+        This experience ended as an incredibly rewarding one as I was able to see these tools deployed to hundreds of company\
+        analysts and engineers. As this was my first software-related job, it also provided a large, established, and\
+        supportive environment for me to begin building important skill sets for my future. 
+        `,
         skills: [
             {
                 label: "Tech",
                 items: [
-                    "React.js",
-                    "Google Apps Script"
-                ]
-            },
-            {
-                label: "HIDDEN",
-                items: [
                     "HTML",
                     "CSS",
-                    "JavaScript"
+                    "JavaScript",
+                    "React.js",
+                    "Google Apps Script"
                 ]
             }
         ]
